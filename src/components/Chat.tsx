37df135f27fc1c22a7181b12515c8558aa755f8e
ezurno <ezurno@gmail.com>
2023-06-13 23:33:50 +0900

    SET: create speech-bubble

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,3 +1,32 @@
+import styled from "styled-components";
+
+const SpeechBubble = styled.div`
+  position: relative;
+  margin: 50px;
+  background: red;
+  border-radius: 10px;
+  ::after {
+    border-top: 15px solid red;
+    border-left: 15px solid transparent;
+    border-right: 0px solid transparent;
+    border-bottom: 0px solid transparent;
+    content: "";
+    position: absolute;
+    top: 10px;
+    left: -15px;
+  }
+`;
+
+const Container = styled.div`
+  width: 50vw;
+`;
+
 export default function Chat() {
-  return <div>Hello</div>;
+  return (
+    <Container>
+      <SpeechBubble>
+        <h1>H</h1>
+      </SpeechBubble>
+    </Container>
+  );
 }
