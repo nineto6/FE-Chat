8f6e423fc96621bdb7549a75789a0bb1ffb044b6
ezurno <ezurno@gmail.com>
2023-06-14 23:03:27 +0900

    ADD: create owner-bubble

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-
 const SpeechBubble = styled.div`
   position: relative;
   margin: 50px;
@@ -13,20 +12,38 @@ const SpeechBubble = styled.div`
     content: "";
     position: absolute;
     top: 10px;
-    left: -15px;
+    left: -14px;
   }
 `;
-
 const Container = styled.div`
   width: 50vw;
 `;
 
+const OwnerBubble = styled.div`
+  position: relative;
+  margin: 50px;
+  background: blue;
+  border-radius: 10px;
+  ::after {
+    border-top: 15px solid blue;
+    border-left: 15px solid transparent;
+    border-right: 15px solid transparent;
+    border-bottom: 0px solid transparent;
+    content: "";
+    position: absolute;
+    top: 10px;
+    right: -15px;
+  }
+`;
 export default function Chat() {
   return (
     <Container>
       <SpeechBubble>
         <h1>H</h1>
       </SpeechBubble>
+      <OwnerBubble>
+        <h1>P</h1>
+      </OwnerBubble>
     </Container>
   );
 }
