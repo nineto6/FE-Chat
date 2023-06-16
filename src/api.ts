4261de64b42fd95ae8659183905e0ce898865972
ezurno <ezurno@gmail.com>
2023-06-16 15:24:56 +0900

    ADD: create login form

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { ISignUpData } from "./pages/SignUp";
 import { useMutation } from "react-query";
+import { IFormData } from "./pages/Login";
 
 export const OnSignUpData = () => {
   return useMutation(SignUpData);
@@ -10,5 +11,29 @@ export const SignUpData = async (data: ISignUpData) => {
     .post(`${process.env.REACT_APP_URL}/api/users/signup`, data)
     .then((res) => {
       console.log(res.data);
+    });
+};
+
+export const OnFormPostData = () => {
+  return useMutation(formPostData);
+};
+export const formPostData = async (data: IFormData) => {
+  return await axios
+    .post(`${process.env.REACT_APP_URL}/api/users/login`, data)
+    .then((response) => {
+      console.log(response);
+      let ACCESS_TOKEN = JSON.stringify(
+        response.headers["access-token"]
+      ); /*.replace(/\"/gi, "")*/
+      // JSON 형식이므로 JSON.stringify 를 사용해 주어야 한다. (*대소문자 주의*)
+      // ACCESS_TOKEN 으로 초기화
+      // console.log(ACCESS_TOKEN);
+      let REFRESH_TOKEN = JSON.stringify(response.headers["refresh-token"]);
+      localStorage.setItem("accessToken", ACCESS_TOKEN);
+      localStorage.setItem("refreshToken", REFRESH_TOKEN);
+      // console.log(localStorage.getItem("accessToken"));
+    })
+    .catch((error) => {
+      console.log(error);
     });
 };
